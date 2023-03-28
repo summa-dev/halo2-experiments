7cd653b8f5e8ae9788532fd42448884b7c054cd6
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-03-28 10:40:55 +0700

    removed Generic Field Type for poseidon hash
    The Poseidon Config is now taking the pasta Field by default while before it was set as a generic. That's require to use that as a chip inside other chips. Especially the configuration of the chip requires to pass the P128Pow5T3 struct which works on the pasta field

--- src/chips/poseidon.rs
+++ src/chips/poseidon.rs
@@ -7,7 +7,7 @@ is already implemented in halo2_gadgets, there is no wrapper chip that makes it 
 // Furthermore it adds an instance column to store the public expected output of the hash
 
 use halo2_gadgets::poseidon::{primitives::*, Hash, Pow5Chip, Pow5Config};
-use halo2_proofs::{arithmetic::FieldExt, circuit::*, plonk::*};
+use halo2_proofs::{arithmetic::FieldExt, circuit::*, plonk::*, halo2curves::pasta::Fp};
 use std::marker::PhantomData;
 
 #[derive(Debug, Clone)]
@@ -15,34 +15,28 @@ use std::marker::PhantomData;
 // WIDTH, RATE and L are const generics for the struct, which represent the width, rate, and number of inputs for the Poseidon hash function, respectively.
 // This means they are values that are known at compile time and can be used to specialize the implementation of the struct.
 // The actual chip provided by halo2_gadgets is added to the parent Chip.
-pub struct PoseidonConfig<F: FieldExt, const WIDTH: usize, const RATE: usize, const L: usize> {
+pub struct PoseidonConfig<const WIDTH: usize, const RATE: usize, const L: usize> {
     hash_inputs: Vec<Column<Advice>>,
     instance: Column<Instance>,
-    pow5_config: Pow5Config<F, WIDTH, RATE>,
+    pow5_config: Pow5Config<Fp, WIDTH, RATE>,
 }
 
 #[derive(Debug, Clone)]
 
 pub struct PoseidonChip<
-    F: FieldExt,
-    S: Spec<F, WIDTH, RATE>,
+    S: Spec<Fp, WIDTH, RATE>,
     const WIDTH: usize,
     const RATE: usize,
     const L: usize,
 > {
-    config: PoseidonConfig<F, WIDTH, RATE, L>,
+    config: PoseidonConfig<WIDTH, RATE, L>,
     _marker: PhantomData<S>,
 }
 
-impl<
-        F: FieldExt,
-        S: Spec<F, WIDTH, RATE>,
-        const WIDTH: usize,
-        const RATE: usize,
-        const L: usize,
-    > PoseidonChip<F, S, WIDTH, RATE, L>
+impl<S: Spec<Fp, WIDTH, RATE>, const WIDTH: usize, const RATE: usize, const L: usize> 
+    PoseidonChip<S, WIDTH, RATE, L>
 {
-    pub fn construct(config: PoseidonConfig<F, WIDTH, RATE, L>) -> Self {
+    pub fn construct(config: PoseidonConfig<WIDTH, RATE, L>) -> Self {
         Self {
             config,
             _marker: PhantomData,
@@ -51,13 +45,13 @@ impl<
 
     // Configuration of the PoseidonChip
     pub fn configure(
-        meta: &mut ConstraintSystem<F>,
+        meta: &mut ConstraintSystem<Fp>,
         state: Vec<Column<Advice>>,
         partial_sbox: Column<Advice>,
         rc_a: Vec<Column<Fixed>>,
         rc_b: Vec<Column<Fixed>>,
         instance: Column<Instance>,
-    ) -> PoseidonConfig<F, WIDTH, RATE, L> {
+    ) -> PoseidonConfig<WIDTH, RATE, L> {
         for i in 0..WIDTH {
             meta.enable_equality(state[i]);
         }
@@ -84,13 +78,13 @@ impl<
     // It uses the pow5_chip to compute the hash
     pub fn hash(
         &self,
-        mut layouter: impl Layouter<F>,
-        inputs: [Value<F>; L],
-    ) -> Result<AssignedCell<F, F>, Error> {
+        mut layouter: impl Layouter<Fp>,
+        inputs: [Value<Fp>; L],
+    ) -> Result<AssignedCell<Fp, Fp>, Error> {
         // Assign values to word_cells by copying it from the cells passed as input
         let hash_input_cells = layouter.assign_region(
             || "load private inputs",
-            |mut region| -> Result<[AssignedCell<F, F>; L], Error> {
+            |mut region| -> Result<[AssignedCell<Fp, Fp>; L], Error> {
                 let result = inputs
                     .iter()
                     .enumerate()
@@ -102,7 +96,7 @@ impl<
                             || x.to_owned(),
                         )
                     })
-                    .collect::<Result<Vec<AssignedCell<F, F>>, Error>>();
+                    .collect::<Result<Vec<AssignedCell<Fp, Fp>>, Error>>();
                 Ok(result?.try_into().unwrap())
             },
         )?;
@@ -119,8 +113,8 @@ impl<
 
     pub fn expose_public(
         &self,
-        mut layouter: impl Layouter<F>,
-        cell: &AssignedCell<F, F>,
+        mut layouter: impl Layouter<Fp>,
+        cell: &AssignedCell<Fp, Fp>,
         row: usize,
     ) -> Result<(), Error> {
         layouter.constrain_instance(cell.cell(), self.config.instance, row)
