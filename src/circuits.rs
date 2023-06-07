a218f63a96d81fc02b63f50717b1c48e16e867ee
Enrico Bottazzi <85900164+enricobottazzi@users.noreply.github.com>
2023-06-07 13:09:51 +0200

    add aggregator

--- src/circuits.rs
+++ src/circuits.rs
@@ -1,4 +1,3 @@
-pub mod hash_v1;
+// pub mod hash_v1;
 pub mod hash_v2;
-pub mod merkle_v3;
-pub mod utils;
+// pub mod utils;
--- src/circuits/hash_v2.rs
+++ src/circuits/hash_v2.rs
@@ -1,12 +1,22 @@
 use super::super::chips::hash_v2::{Hash2Chip, Hash2Config};
 use halo2_proofs::halo2curves::bn256::Fr as Fp;
-
 use halo2_proofs::{circuit::*, plonk::*};
+use snark_verifier_sdk::CircuitExt;
 
 #[derive(Default)]
 struct Hash2Circuit<Fp> {
-    pub a: Value<Fp>,
-    pub b: Value<Fp>,
+    pub a: Fp,
+    pub b: Fp,
+}
+
+impl CircuitExt<Fp> for Hash2Circuit<Fp> {
+    fn num_instance(&self) -> Vec<usize> {
+        vec![2]
+    }
+
+    fn instances(&self) -> Vec<Vec<Fp>> {
+        vec![vec![self.a, self.b]]
+    }
 }
 
 impl Circuit<Fp> for Hash2Circuit<Fp> {
@@ -32,8 +42,8 @@ impl Circuit<Fp> for Hash2Circuit<Fp> {
         mut layouter: impl Layouter<Fp>,
     ) -> Result<(), Error> {
         let chip = Hash2Chip::construct(config);
-        let a = chip.load_private(layouter.namespace(|| "load a"), self.a)?;
-        let b = chip.load_private(layouter.namespace(|| "load b"), self.b)?;
+        let a = chip.load_private(layouter.namespace(|| "load a"), Value::known(self.a))?;
+        let b = chip.load_private(layouter.namespace(|| "load b"), Value::known(self.b))?;
         let c = chip.hash(layouter.namespace(|| "load row"), a, b.clone())?;
         chip.expose_public(layouter.namespace(|| "hash output check"), &c, 0)?;
         chip.expose_public(layouter.namespace(|| "b check"), &b, 1)?;
@@ -45,24 +55,50 @@ impl Circuit<Fp> for Hash2Circuit<Fp> {
 #[cfg(test)]
 mod tests {
     use super::Hash2Circuit;
-    use crate::circuits::utils::{evm_verify, gen_evm_verifier, gen_proof};
-    use halo2_proofs::plonk::{keygen_pk, keygen_vk};
+    // use crate::circuits::utils::{evm_verify, gen_evm_verifier, gen_proof};
+    use ark_std::{end_timer, start_timer};
+    use halo2_proofs::circuit;
+    use halo2_proofs::plonk::{keygen_pk, keygen_vk, Circuit};
+    use halo2_proofs::poly::commitment::Params;
     use halo2_proofs::{
-        circuit::Value,
         dev::MockProver,
         halo2curves::bn256::{Bn256, Fr as Fp},
         poly::kzg::commitment::ParamsKZG,
     };
-    use rand::rngs::OsRng;
+    use snark_verifier_sdk::CircuitExt;
+    use snark_verifier_sdk::SHPLONK;
+    use snark_verifier_sdk::{
+        evm::{evm_verify, gen_evm_proof_shplonk, gen_evm_verifier_shplonk},
+        gen_pk,
+        halo2::{aggregation::AggregationCircuit, gen_snark_shplonk, gen_srs},
+        Snark,
+    };
+    use std::fs::File;
+    use std::path::Path;
+
+    fn gen_application_snark(params: &ParamsKZG<Bn256>) -> Snark {
+        let a = Fp::from(2);
+        let b = Fp::from(7);
+
+        let circuit = Hash2Circuit::default();
+
+        // let circuit_b = Hash2Circuit { a, b };
+
+        let vk = keygen_vk(params, &circuit).unwrap();
+        let pk = keygen_pk(params, vk, &circuit).unwrap();
+
+        // if I pass circuit to gen_snark_shplonk, it fails...
+        gen_snark_shplonk(params, &pk, circuit, None::<&str>)
+    }
 
     #[test]
     fn test_hash_2() {
         let k = 4;
 
         // successful case
-        let a = Value::known(Fp::from(2));
-        let b = Value::known(Fp::from(7));
-        let public_inputs = vec![Fp::from(9)];
+        let a = Fp::from(2);
+        let b = Fp::from(7);
+        let public_inputs = vec![Fp::from(9), Fp::from(7)];
         let circuit = Hash2Circuit { a, b };
         let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
         assert_eq!(prover.verify(), Ok(()));
@@ -74,24 +110,47 @@ mod tests {
     }
 
     #[test]
-    fn test_hash_2_on_chain() {
+    fn test_hash_2_aggregation() {
         let k = 4;
-        let a = Value::known(Fp::from(2));
-        let b = Value::known(Fp::from(7));
-        let circuit = Hash2Circuit { a, b };
+        let params_app = gen_srs(k);
 
-        let params = ParamsKZG::<Bn256>::setup(k, OsRng);
+        let snarks = [(); 1].map(|_| gen_application_snark(&params_app));
 
-        let vk = keygen_vk(&params, &circuit).unwrap();
+        let ptau_path = format!("ptau/hermez-raw-{}", 22);
+        let mut params_fs = File::open(ptau_path).expect("couldn't load params");
+        let params = ParamsKZG::<Bn256>::read(&mut params_fs).expect("Failed to read params");
 
-        let pk = keygen_pk(&params, vk.clone(), &circuit).unwrap();
+        // Here I'm creating a new circuit I guess!
+        let agg_circuit = AggregationCircuit::<SHPLONK>::new(&params, snarks);
 
-        let deployment_code = gen_evm_verifier(&params, &vk, vec![2]);
+        // Generating artifacts for the agg circuit
+        let start0 = start_timer!(|| "gen vk & pk");
+        let pk = gen_pk(
+            &params,
+            &agg_circuit.without_witnesses(),
+            Some(Path::new("./examples/agg.pk")),
+        );
+        end_timer!(start0);
 
-        let instances = vec![vec![Fp::from(9), Fp::from(7)]];
+        std::fs::remove_file("./examples/agg.snark").unwrap_or_default();
+        let _snark = gen_snark_shplonk(
+            &params,
+            &pk,
+            agg_circuit.clone(),
+            Some(Path::new("./examples/agg.snark")),
+        );
 
-        let proof = gen_proof(&params, &pk, circuit, instances.clone());
+        let num_instances = agg_circuit.num_instance();
+        let instances = agg_circuit.instances();
+        let proof_calldata = gen_evm_proof_shplonk(&params, &pk, agg_circuit, instances.clone());
 
-        evm_verify(deployment_code, instances, proof);
+        let deployment_code = gen_evm_verifier_shplonk::<AggregationCircuit<SHPLONK>>(
+            &params,
+            pk.get_vk(),
+            num_instances,
+            Some(Path::new("./examples/standard_plonk.yul")),
+        );
+
+        evm_verify(deployment_code, instances, proof_calldata);
     }
 }
